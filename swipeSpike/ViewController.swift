fe2149bd72eee3b5ffdf15b933208b5b01e0388d
u16suzu <u16suzu@gmail.com>
2016-01-08 19:44:46 +0900

    1st

---
+++ swipeSpike/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  swipeSpike
+//
+//  Created by u16suzu on 2016/01/08.
+//  Copyright © 2016年 IGNIS LTD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    var tableView = UITableView.init()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        view.addSubview(tableView)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+
+    // MARK: - UITableViewDelegate
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+    
+    // MARK: - UITableViewDataSource
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 50
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell.init()
+        cell.textLabel?.text = "cell\( String(indexPath.row) )"
+        
+        return cell
+    }
+}
+
